6a0d3224565a8b434f91aee43e77b901cfb01e02
Sreehari <126333578+thesrhari@users.noreply.github.com>
2024-08-05 21:57:12 +0530

    Create styles.css, update index.html & script.js
    - Added `styles.css` for site styling and responsiveness
    - Updated `index.html` to work with styles.css
    - Modified `script.js` to remove unnecessary code. Added function to clear output field when clearing input field

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+// Encoding and decoding happens here
 function caesar(text, shift, direction){
     let caesar_text = "";
     shift = Number(shift);
+
     if (direction == "decode"){
         shift *= -1;
     }
@@ -13,9 +15,11 @@ function caesar(text, shift, direction){
             const position = alphabet.indexOf(letter);
             const shifted_position = position + shift;
             const alphabet_length = alphabet.length;
+
+            // Calling mod() func
             const new_position = mod(shifted_position, alphabet_length);
+
             caesar_text += alphabet[new_position];
-            console.log(position + shift);
         }
         else{
             caesar_text += letter
@@ -25,15 +29,17 @@ function caesar(text, shift, direction){
     output.value = caesar_text;
 }
 
+// Returns remainder (Also works with negative numbers)
 function mod(a, b){
     return ((a % b) + b) % b;
 }
 
-
+// Collects input and calls caesar() function
 function getValue(){
     let plain_text = document.getElementById("plain_text").value;
     let shift_amount = document.getElementById("shift_amount").value;
     let direction_type;
+
     if (document.getElementById("encode").checked){
         direction_type = document.getElementById("encode").value;
     }
@@ -48,3 +54,18 @@ function getValue(){
         caesar(plain_text, shift_amount, direction_type);
     }
 }
+
+
+// Checking and clearing output field if input field is empty
+function checkIfEmpty() {
+    const textarea = document.getElementById('plain_text');
+    if (textarea.value == "") {
+        const output_field = document.getElementById("output");
+        output_field.value = "";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('plain_text');
+    textarea.addEventListener('input', checkIfEmpty);
+});
